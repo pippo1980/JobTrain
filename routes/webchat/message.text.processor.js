f9ee9af231550063ad06af5ddd22516d9a22c4b7
pippo <philip.kissme@gmail.com>
2014-08-18 10:59:37 +0800

    welcome

--- routes/webchat/message.text.processor.js
+++ routes/webchat/message.text.processor.js
@@ -8,11 +8,11 @@ message.on("subscribe", function (context) {
     var message = context['message'];
     var response = context['response'];
 
-    var welcom_msg = {'xml': {'ToUserName': message['FromUserName'], 'FromUserName': message['ToUserName'], 'CreateTime': new Date().getTime(), 'MsgType': 'text', 'Content': '感谢您关注包就业'}};
+    var welcome_msg = {'xml': {'ToUserName': message['FromUserName'], 'FromUserName': message['ToUserName'], 'CreateTime': new Date().getTime(), 'MsgType': 'text', 'Content': '感谢您关注包就业'}};
     var builder = new xml2js.Builder();
     var xml = builder.buildObject(webcom_msg);
 
-    logger.debug("[%s]:%s", __filename, welcom_msg);
+    logger.debug("[%s]:%s", __filename, welcome_msg);
     response.send(xml);
     response.end();
 });
