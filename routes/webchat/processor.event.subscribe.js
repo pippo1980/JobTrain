44192dcba217230ba99f9ba97a555d03de9748ce
pippo <philip.kissme@gmail.com>
2014-08-18 13:56:19 +0800

    默认body作为response解析

--- routes/webchat/processor.event.subscribe.js
+++ routes/webchat/processor.event.subscribe.js
@@ -1,20 +1,21 @@
 /**
  * Created by pippo on 14-8-12.
  */
-var xml2js = require('xml2js');
 var logger = require("../../logger").get(__filename);
 var processor = require("./processor");
+var welcome = require("./reply/welcome");
 
 processor.on("subscribe", function (context) {
     var xml = context['xml'];
     var from = xml['FromUserName'];
     logger.debug({file: __filename, info: 'user subscribe us', user: from})
 
-    var welcome = {'xml': {'ToUserName': from, 'FromUserName': xml['ToUserName'], 'CreateTime': new Date().getTime(), 'MsgType': 'text', 'Content': '感谢您关注包就业'}};
+    /*因为是回复,所以from/to要倒过来*/
+    var welcome = welcome.create(xml['ToUserName'], from)
     logger.debug({file: __filename, info: "return welcome msg", welcome: welcome});
 
     var response = context['response'];
-    response.send(new xml2js.Builder().buildObject(welcome));
+    response.send(welcome);
     response.end();
 });
 
