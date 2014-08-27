7da7a948810f3a6827c52225616d35d4c9009ce8
pippo <philip.kissme@gmail.com>
2014-08-27 19:07:12 +0800

    前端页面美化

--- routes/mobile/train.js
+++ routes/mobile/train.js
@@ -6,6 +6,8 @@ var Train = require("../../model/Train");
 module.exports.init = function (router) {
     router.get("/m/trains", trains);
     router.get("/m/train/synopsis", synopsis);
+    router.get("/m/train/detail", detail);
+
 }
 
 function trains(req, res) {
@@ -17,6 +19,17 @@ function trains(req, res) {
 }
 
 function synopsis(req, res) {
+    var train = new Train();
+    train.train_type = req.param("type");
+    train.price = req.param("price");
+
+    train.getByType(function (success) {
+        //console.log(train)
+        res.render("mobile/train_synopsis", {train: train});
+    });
+}
+
+function detail(req, res) {
     var train = new Train();
     train.train_type = req.param("type");
     train.price = req.param("price");
